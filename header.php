7812c7128b3a9cf4d4f2f6baf0155eb62ceff1ea
Dani <77969784+danielttttttt@users.noreply.github.com>
2025-01-12 05:16:29 -0800

    header

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+
+<header class="header">
+    <div class="header-1">
+        <div class="flex">
+            <a href="home.php" class="logo">NONSENSE BOOK-STORE</a>
+            <p style="margin-left:auto;"> <a href="login.php">login</a> | <a href="register.php">register</a> </p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-2">
+        <div class="flex">
+            <nav class="navbar">
+                <a href="home.php">home</a>
+                <a href="about.php">about</a>
+                <a href="shop.php">shop</a>
+                <a href="contact.php">contact</a>
+                <a href="orders.php">orders</a>
+                <a href="wishlist.php">wishlist</a> 
+                <a href="feedback.php">feedback</a> 
+                <a href="book_listing.php">book listing</a> 
+                <a href="logout.php">logout</a> 
+            </nav>
+            <div class="icons">
+                <div id="menu-btn" class="fas fa-bars"></div>
+                <a href="search_page.php" class="fas fa-search"></a>
+                <div id="user-btn" class="fas fa-user"></div>
+                <a href="cart.php"> <i class="fas fa-shopping-cart"></i> </a> 
+            </div>
+
+            </div>
+        </div>
+
+</header>
+
+</body>
+</html>
