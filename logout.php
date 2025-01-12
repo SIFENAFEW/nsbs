0ef6a2f3f1fadb91870056b71fa59ad382b43405
Dani <77969784+danielttttttt@users.noreply.github.com>
2025-01-12 05:35:33 -0800

    fixed logout it will go to the login when you touch it

--- logout.php
+++ logout.php
@@ -1,22 +1,11 @@
-<?php 
-session_start();
-session_destroy();
-header("Location: login.php");
-exit();
-?>
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logout</title>
-    <link rel="stylesheet" href="signup.css">
-</head>
-<body>
-    <div class="signup-container">
-        <h2>You have been logged out</h2>
-        <p>Thank you for using our service. You can <a href="login.php">login again</a> anytime.</p>
-    </div>
-</body>
-</html>
+include '../dbs/db_conn.php';
+
+session_start();
+session_unset();
+session_destroy();
+
+header('location:login.php');
+
+?>
