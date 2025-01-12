35997e5ab647c3258247fecc1673a15900066dee
Dani <77969784+danielttttttt@users.noreply.github.com>
2025-01-12 08:28:12 -0800

    footer

---
+++ footer.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="footer.css">
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="wishlist.php">wishlist</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +251-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +251-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> NBS@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Addis, Ethiopia - 400104 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>NONSENSE BOOK-STOREr</span> </p>
+
+</section>
