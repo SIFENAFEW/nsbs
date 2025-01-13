fe591ec681c1960c72643ab4007c9bdc9593aa69
Dani <77969784+danielttttttt@users.noreply.github.com>
2025-01-13 02:33:00 -0800

    updated

--- wishlist.php
+++ wishlist.php
@@ -1,5 +1,5 @@
 <?php 
-include 'dbs/db_conn.php';
+include 'config.php'; 
 
 session_start(); 
 
@@ -15,13 +15,14 @@ if(isset($_POST['send'])){
     $author = mysqli_real_escape_string($conn, $_POST['author']); 
     $email = mysqli_real_escape_string($conn, $_POST['email']); 
     $comment = mysqli_real_escape_string($conn, $_POST['comment']); 
+    $genre = mysqli_real_escape_string($conn, $_POST['genre']); 
 
     $select_book = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE title = '$title' AND author = '$author' AND email = '$email'") or die('query failed'); 
 
     if(mysqli_num_rows($select_book) > 0){ 
         $message[] = 'This book is already in your wishlist!'; 
     }else{ 
-        mysqli_query($conn, "INSERT INTO `wishlist`(user_id, title, author, email, comment) VALUES('$user_id', '$title', '$author', '$email', '$comment')") or die('query failed'); 
+        mysqli_query($conn, "INSERT INTO `wishlist`(user_id, title, author, email, comment, genre) VALUES('$user_id', '$title', '$author', '$email', '$comment', '$genre')") or die('query failed'); 
         $message[] = 'Book added to your wishlist successfully!'; 
     } 
 
@@ -56,11 +57,26 @@ if(isset($_POST['send'])){
 
     <form action="" method="post"> 
         <h3>Add a Book to Wishlist</h3> 
-        <input type="text" name="title" required placeholder="Enter book title" class="box"> 
-        <input type="text" name="author" required placeholder="Enter author's name" class="box"> 
-        <input type="email" name="email" required placeholder="Enter your email" class="box"> 
-        <textarea name="comment" class="box" placeholder="Enter any comment" id="" cols="30" rows="10"></textarea> 
-        <input type="submit" value="Add to Wishlist" name="send" class="btn"> 
+        <input type="text" name="title" required placeholder="What’s the magical book you have in mind?" class="box"> 
+        <input type="text" name="author" required placeholder="Who’s the brilliant author?" class="box"> 
+        <input type="email" name="email" required placeholder="Where can we send updates about your wish?" class="box"> 
+        <textarea name="comment" class="box" placeholder="Tell us why this book should be added (optional)" cols="30" rows="10"></textarea> 
+
+        <!-- Genre picker -->
+        <select name="genre" class="box" required>
+            <option value="" disabled selected>Pick a genre or mood</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Romance">Romance</option>
+            <option value="Sci-Fi">Sci-Fi</option>
+            <option value="Mystery">Mystery</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Non-fiction">Non-fiction</option>
+            <option value="Uplifting">Uplifting</option>
+            <option value="Cozy">Cozy</option>
+        </select> 
+
+        <!-- Fun Submit Button -->
+        <input type="submit" value="Grant My Wish!" name="send" class="btn"> 
     </form> 
 
 </section>
